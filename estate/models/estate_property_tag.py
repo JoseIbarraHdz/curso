9ff1b6d6673015b25768d6498673b0d81219b391
JoseIbarraHdz <josei@vauxoo.com>
2024-08-21 17:08:46 +0000

    [IMP] estate: computed fields are now working
    The relation between models, computed fields and buttons are now on function..

---
+++ estate/models/estate_property_tag.py
@@ -0,0 +1,8 @@
+from odoo import fields, models
+
+
+class EstatePropertyTag(models.Model):
+    _name = "estate.property.tag"
+    _description = "The model estate.property.tag has no access rules, consider adding one."
+
+    name = fields.Char(required=True)
--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -1,6 +1,8 @@
 from odoo import fields, models
 
+
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
+    _description = "Estate Property TYpe"
 
     name = fields.Char(required=True)
